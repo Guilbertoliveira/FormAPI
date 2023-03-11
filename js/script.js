2e6245a659b576ccccbc39ce1b3a2e0bfa6a6418
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-03-10 23:34:15 -0300

    update code js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-.then( (resposta) => 
-{resposta.json().then((r)=> {
-    console.log(r);
-})
-});
+var consultaCEP = fetch('https://viacep.com.br/ws/01001250/json/') //fetch é a requisição
+.then( resposta => resposta.json()) //realizando a conversão da promessa para json
+.then(r=> {
+    if(r.erro){ //tratando erro que não cai no 400, numero de cep coerente, porém invalido
+       throw Error('Esse cep não existe');
+    } else {
+    console.log(r)}
+    }) // pegando conversão e demonstrando na tela
+.catch(erro => console.log(erro)); //criando erro para demonstrar na tela, (erro padrao), erro 400 bad request
+
+
 
 console.log(consultaCEP)
 
